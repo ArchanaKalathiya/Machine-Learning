015bb5042aae967d4121d9b96cbda27f642ac577
Archana Kalathiya <94705812+ArchanaKalathiya@users.noreply.github.com>
2023-05-06 13:25:27 +0530

    Sales Prediction Project

--- Logistic Regression/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-# train_test_split(X,Y,test_size=0.25, random_state=0) #useful for validation , 25% of the data given to test 
-# model.fit(X_train,Y_train)
-# result = model.predict(sc.transform(newCust)
